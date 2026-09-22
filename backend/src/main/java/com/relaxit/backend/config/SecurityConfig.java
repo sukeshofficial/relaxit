1244b0416d975ef96cee01317dead0e4f3ae0743
sukeshofficial <sukesh.official.2006@gmail.com>
2026-09-22 23:29:52 +0530

    update: security

--- backend/src/main/java/com/relaxit/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/relaxit/backend/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .cors(cors -> cors.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
